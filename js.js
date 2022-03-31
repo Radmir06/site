028605dd85f084fcb7ed102afe0dfd244cd08d97
Radmir06 <102294943+Radmir06@users.noreply.github.com>
2022-03-31 15:54:55 +0500

    Add files via upload

--- js.js
+++ js.js
@@ -1,12 +1,43 @@
 let albums = document.getElementsByClassName('albums') 
-let box = document.getElementsByClassName('box')
-function open(a){  
-    for(let i = 0; i < albums.length; i++){
-        if(i==1){
-            albums[i].style.display = 'flex'
-        }
-        else{
-            albums[i].style.display = 'none'
+function OpenMod(a){
+    if(albums[a].style.maxHeight == '0px'){
+        for(let i = 0; i < albums.length; i++){
+            if(i==a){
+                albums[a].style.maxHeight = '5000px'
+            }
+            else{
+                albums[i].style.maxHeight = '0px'
+           }
         }
     }
+    else{
+        for(let i = 0; i < albums.length; i++){
+            if(i==a){
+                albums[a].style.maxHeight = '0px'
+            }
+            
+        }
+
+    }
 }   
+let ispol = document.getElementById('spisok')
+let vseispol = document.getElementsByClassName('vse_ispol')
+function OpenIspol() {
+    if(ispol.style.display=='flex'){
+        ispol.style.display='none'
+        vseispol[0].style.height = '70px'
+    }
+    else{
+        ispol.style.display='flex'
+        vseispol[0].style.height = '218px'
+    }
+}
+let menu = document.getElementsByClassName('menu1')
+function nav() {
+    if(menu[0].style.height=='0px'){
+        menu[0].style.height='200px'
+    }
+    else{
+        menu[0].style.height='0px'
+    }
+}
